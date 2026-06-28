195401abdf80e22750041c7ee89802cc7d943f64
HemannB <brunohemann@gmail.com>
2026-06-27 22:18:58 -0300

    feat: add UpcomingTaskDetailPanel typed to UpcomingTaskDetailViewModel
    Signed-off-by: HemannB <brunohemann@gmail.com>

---
+++ Zeno.App/Views/UpcomingTaskDetailPanel.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Zeno.App.Views;
+
+public partial class UpcomingTaskDetailPanel : UserControl
+{
+    public UpcomingTaskDetailPanel()
+    {
+        InitializeComponent();
+    }
+}
