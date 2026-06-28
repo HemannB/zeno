14e004095042859973365836a06d6355572d7247
HemannB <brunohemann@gmail.com>
2026-06-27 21:47:10 -0300

    feat: add ProjectTaskDetailPanel typed to ProjectTaskDetailViewModel

---
+++ Zeno.App/Views/ProjectTaskDetailPanel.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Zeno.App.Views;
+
+public partial class ProjectTaskDetailPanel : UserControl
+{
+    public ProjectTaskDetailPanel()
+    {
+        InitializeComponent();
+    }
+}
